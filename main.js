e6ad78e9731d817778dd0f3406fabae7479dedf3
crummers <marc.carruthers@hotmail.com>
2018-12-20 16:58:47 +0000

    Created variables in main.js for template elements and to access the html of them

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const templateElement = document.getElementById('templateHB');
+
+const templateSource = templateElement.innerHTML;
+
+
 const context = {
   title: 'Welcome to Musicon',
   body: 'Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
